5139027621b30df72fd5419c3f5d6bd5bba03e32
dpvaughan <david.vaughan@mrc-epid.cam.ac.uk>
2016-01-18 10:48:46 +0000

    this is a test

--- GeneFinder.java
+++ GeneFinder.java
@@ -2,7 +2,20 @@ public class GeneFinder{
 
   public static void main( String args[]){
 
-    System.out.println("Started");
+    String dna = "AATGCTAGTTTAAATCTGA";
+    String[] stopCodons = new String[3];
+    String propProtein = "";
+
+    stopCodons[0] = "TAG";
+    stopCodons[1] = "TGA";
+    stopCodons[2] = "TAA";
+
+    TagFinder tag = new TagFinder(dna);
+
+    for(int i = 0; i < stopCodons.length; i++){
+      propProtein = tag.findProtein(stopCodons[i]);
+      System.out.println("End Codon " + stopCodons[i] + " - " + propProtein);
+    }
 
   }
 
