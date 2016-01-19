0ee001c14bdadf1d25a4aa0a9d1865e50c53204f
dpvaughan <david.vaughan@mrc-epid.cam.ac.uk>
2016-01-19 09:35:06 +0000

    new files

--- WebLinks.java
+++ WebLinks.java
@@ -1,7 +1,12 @@
+import java.edu.duke;
+
 public class WebLinks{
 
   public static void main(String args[]){
     System.out.println("This is a test");
+
+    StorageResource store = new StorageResource();
+
   }
 
 }
